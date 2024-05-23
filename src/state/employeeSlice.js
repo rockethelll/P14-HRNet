8288b50ba18c4936ced75ea79d310c4725c48fe0
rockethelll <51137055+rockethelll@users.noreply.github.com>
2024-05-23 15:36:01 +0200

    add reduxtoolkit, add store, add employeeReducer, add Provider

---
+++ src/state/employeeSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  employees: [],
+};
+
+export const employeeSlice = createSlice({
+  name: 'employee',
+  initialState,
+  reducers: {
+    addEmployee: (state, action) => {
+      state.employees.push(action.payload);
+    },
+  },
+});
+
+export const { addEmployee } = employeeSlice.actions;
+export default employeeSlice.reducer;
