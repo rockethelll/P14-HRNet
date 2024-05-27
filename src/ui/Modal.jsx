96a5142d09f27801e7de44cbaa75d4c21c03d33e
rockethelll <51137055+rockethelll@users.noreply.github.com>
2024-05-27 14:02:48 +0200

    add closeModal to the entire modal for close when click anywhere on the modal

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,7 +1,17 @@
 const Modal = ({ closeModal }) => {
+  const stopPropagation = (e) => {
+    e.stopPropagation();
+  };
+
   return (
-    <div className='fixed z-10 flex h-screen w-screen items-center justify-center overflow-auto bg-black/75 text-center'>
-      <div className='relative z-20 w-[500px] translate-y-[-50%] rounded-lg bg-white px-8 py-4 text-left font-semibold	shadow-custom'>
+    <div
+      className='fixed z-10 flex h-screen w-screen items-center justify-center overflow-auto bg-black/75 text-center'
+      onClick={closeModal}
+    >
+      <div
+        className='relative z-20 w-[500px] translate-y-[-50%] rounded-lg bg-white px-8 py-4 text-left font-semibold	shadow-custom'
+        onClick={stopPropagation}
+      >
         Employee Created!
         <button
           className='absolute right-[-12.5px] top-[-12.5px] h-8 w-8 bg-close bg-cover'
