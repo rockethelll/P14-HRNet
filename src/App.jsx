e71683f041a13b89b974e4516433455bf040bbe5
rockethelll <51137055+rockethelll@users.noreply.github.com>
2024-06-05 11:51:14 +0200

    change modal version, change modal location for use createPortal

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import Modal from 'modal-oc-p14';
-
 import Navbar from '@/components/Navbar';
 import Router from '@/Router/Router';
-import useModal from '@/utils/useModal';
 
 const App = () => {
-  // Destructure the isModalOpen and handleCloseModal functions from the useModal hook
-  const { isModalOpen, handleCloseModal } = useModal();
   return (
     <>
-      {isModalOpen ? (
-        <Modal
-          isOpen={isModalOpen}
-          content='Employee Created'
-          closeModal={handleCloseModal}
-        />
-      ) : null}
       <Navbar />
       <Router />
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,28 @@
+import Modal from 'modal-oc-p14';
+import { createPortal } from 'react-dom';
+
 import Form from '@/components/Form/Form';
 import Container from '@/ui/Container';
 import Title from '@/ui/Title';
+import useModal from '@/utils/useModal';
 
 const Home = () => {
+  // Destructure the isModalOpen and handleCloseModal functions from the useModal hook
+  const { isModalOpen, handleCloseModal } = useModal();
+
   return (
     <Container>
       <Title title='Create Employee' />
       <Form />
+      {isModalOpen &&
+        createPortal(
+          <Modal
+            isOpen={isModalOpen}
+            content='Employee Created'
+            closeModal={handleCloseModal}
+          />,
+          document.body,
+        )}
     </Container>
   );
 };
